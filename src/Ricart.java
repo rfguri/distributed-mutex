c67513af8c354771c4c1734ada09d81c024295bc
Roger Fernandez Guri <rfguri@gmail.com>
2014-11-17 18:42:15 +0100

    Fix a problem with critical section access on network delay

--- src/Ricart.java
+++ src/Ricart.java
@@ -19,36 +19,40 @@ public class Ricart extends Process {
 			Message m = cm.getM(pid, i);
 			if (m != null) {
 				if (m.getM().equals(Message.Start)) {
-					m.brodcastMessage(pid, p, cm, new Message(Message.Request, m.getT()));
+					this.setExecuted(false);
+					t = pid;
+					m.brodcastMessage(pid, p, cm, new Message(Message.Request, t));
 				} 
 				else if (m.getM().equals(Message.Request)) {
 					if (pid != i) {
-						if (DEBUG) System.out.printf("[%d] (%d) %s [%d, %d, %d]\n", System.currentTimeMillis(), pid, m.getM(), q[0], q[1], q[2]);
-						if (!this.isExecuted()) {
-							if (m.getT() < t) m.sendMessage(i, pid, cm, new Message(Message.Acknowledge, t));
-							else if (m.getT() == t && i < pid) m.sendMessage(i, pid, cm, new Message(Message.Acknowledge, t));
-							else q[i - 2] = 1;
-						} else m.sendMessage(i, pid, cm, new Message(Message.Acknowledge, t));
+						if (DEBUG) System.out.printf("[%d] (%d,%d) %s from (%d,%d) [%d, %d, %d]\n", System.currentTimeMillis(), pid, t, m.getM(), i, m.getT(), q[0], q[1], q[2]);
+						if (m.getT() < t) m.sendMessage(i, pid, cm, new Message(Message.Acknowledge, t));
+						else if (m.getT() == t && i < pid) m.sendMessage(i, pid, cm, new Message(Message.Acknowledge, t));
+						else q[i-2] = 1;
 					}
 				}
 				else if (m.getM().equals(Message.Acknowledge)) {
-					if (DEBUG) System.out.printf("[%d] (%d) %s [%d, %d, %d]\n", System.currentTimeMillis(), pid, m.getM(), q[0], q[1], q[2]);
+					int index = pid - 2;
+					if (DEBUG) System.out.printf("[%d] (%d,%d) %s from (%d,%d) [%d, %d, %d] | acc: %d\n", System.currentTimeMillis(), pid, t, m.getM(), i, m.getT(), q[0], q[1], q[2], acc);
 					if (acc == 2) {
-						acc = 1;
-						if (DEBUG) for (int j = 0; j < 10; j++) System.out.printf("[%d] (%d) I'm thread A%d\n", System.currentTimeMillis(), pid, pid - 2);
-						if (!DEBUG) for (int j = 0; j < 10; j++) System.out.printf("I'm thread A%d\n", pid - 2);
-						executed = true;
-						cm.setM(1, pid, new Message(Message.Finish, t));
+						if (DEBUG) for (int j = 0; j < 10; j++) {
+							System.out.printf("[%d] (%d) I'm thread A%d\n", System.currentTimeMillis(), pid, index);
+							//try { Thread.sleep(10); } catch (InterruptedException e) {}
+						}
+						if (!DEBUG) for (int j = 0; j < 10; j++) {
+							System.out.printf("I'm thread A%d\n", index);
+							//try { Thread.sleep(10); } catch (InterruptedException e) {}
+						}
 						for (int j = 0; j < q.length; j++) {
 							if (q[j] == 1) {
 								m.sendMessage(j + 2, pid, cm, new Message(Message.Acknowledge, t));
 								q[j] = 0;
-							}	
+							}
 						}
-					} 
-					else {
-						acc++;
-					}
+						cm.setM(1, pid, new Message(Message.Finish, t));
+						if (DEBUG) System.out.printf("[%d] (%d,%d) Finished to 1\n", System.currentTimeMillis(), pid, t);
+						acc = 1;
+					} else acc++;
 				}
 			}
 		}
